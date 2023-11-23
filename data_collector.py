fd00e86d7672c6c957827a3a6628b8dd3fd62740
omygro <111525460+omygro@users.noreply.github.com>
2023-11-23 16:28:57 +0530

    New commit

--- data_collector.py
+++ data_collector.py
@@ -2,6 +2,24 @@ import pandas as pd
 import requests
 from datetime import datetime
 
+def calculate_rsi(data, window=14):
+    delta = data['price'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    return 100 - (100 / (1 + rs))
+
+def preprocess_data(data):
+    data['SMA_15'] = data['price'].rolling(window=15).mean()
+    data['EMA_15'] = data['price'].ewm(span=15, adjust=False).mean()
+    data['RSI'] = calculate_rsi(data)
+    data['target'] = data['price'].shift(-1)  # Example: next period's price
+
+    # Debugging: Print to check if calculations are done
+    print("Data after preprocessing:", data.head())
+
+    return data
+
 def collect_historical_data(crypto_id, api_key, start_date, end_date):
     url = 'https://min-api.cryptocompare.com/data/v2/histoday'
     params = {
@@ -37,9 +55,9 @@ def append_data_to_csv(new_data, file_name):
         new_data.to_csv(file_name, index=False)
 
 if __name__ == "__main__":
-    API_KEY = 'your_api_key_here'
+    API_KEY = '3b311037957c48cff5cf685396cfff76952c1afd5c6fa18ba5cc124b0b4c5095'
     crypto_id = 'BTC'
-    start_date = datetime(2012, 1, 1)  # Adjust the start date as needed
+    start_date = datetime(2008, 1, 1)  # Adjust the start date as needed
     end_date = datetime.now()  # Current date as the end date
 
     new_data = collect_historical_data(crypto_id, API_KEY, start_date, end_date)
